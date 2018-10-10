36e02ed2d994529a7b356406346a3724f360d303
Gtosta96 <gabrieltosta3@gmail.com>
2018-10-10 10:10:42 -0300

    added var / let / const and changed iterators

--- es2015/iterators.js
+++ es2015/iterators.js
@@ -1,3 +1,31 @@
+// const rawIterator = {
+//   [Symbol.iterator]: () => {
+//     return {
+//       next: () => {
+//         const randomNumber = Math.floor(Math.random() * 100 + 1);
+
+//         if (randomNumber > 80) {
+//           return {
+//             value: undefined,
+//             done: true,
+//           }
+//         };
+
+//         return {
+//           value: randomNumber,
+//           done: false,
+//         }
+//       }
+//     }
+//   }
+// }
+
+// for (const it of rawIterator) {
+//   console.log(it);
+// }
+
+/* --- */
+
 const array = [1, 3, 6, 9];
 const iterator = array[Symbol.iterator]();
 
---
+++ es2015/varLetCost.js
@@ -0,0 +1,16 @@
+// console.log(varVariable);
+// console.log(letVariable);
+
+// var varVariable;
+// let letVariable;
+
+// for (let i = 0; i < 10; i++) {
+//   setTimeout(() => console.log(`i --> ${i}`), 100 * i);
+// }
+
+/* ---- */
+
+// const constVariable = null;
+
+// constVariable = 'testValue';
+// console.log(constVariable);
